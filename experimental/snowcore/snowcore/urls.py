678f1b3d3bf02071777b83f3f751752045b78bfb
Caitlyn <caitlynrosestewart@gmail.com>
2022-01-25 12:07:42 +0000

    setup basic index page

--- experimental/snowcore/snowcore/urls.py
+++ experimental/snowcore/snowcore/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls.conf import include
+
 
 urlpatterns = [
+    path('skiing/', include("skiing.urls")),
     path('admin/', admin.site.urls),
 ]
