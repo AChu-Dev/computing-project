ca750959fd518a1d86e8cc8c2ef6d282c2923739
DARAEDom <4romad67@solent.ac.uk>
2022-01-28 21:59:54 +0000

    API: added ability to set custom timelines

--- experimental/API_test/api.py
+++ experimental/API_test/api.py
@@ -1,16 +1,21 @@
 import os
 from dotenv import load_dotenv
 import pandas as pd
+import datetime
 
-def main():
+def main(start_date = 0, end_date = 0):
     try:
         load_dotenv()
         key = os.getenv("API_KEY")
     except Exception:
         print("Exception has occured", Exception)
+
+    if end_date == 0 and start_date == 0:
+        start_date = datetime.datetime.now() - datetime.timedelta(days=30)
+        start_date = start_date.strftime("%Y-%m-%d")
+        end_date = datetime.datetime.now().strftime("%Y-%m-%d")
     
-    url = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/Southampton/2022-01-26/2022-02-11?unitGroup=metric&elements=datetime%2CdatetimeEpoch%2Cname%2Caddress%2Clatitude%2Clongitude%2Ctempmax%2Ctempmin%2Ctemp%2Cfeelslikemax%2Cfeelslikemin%2Cfeelslike%2Chumidity%2Cprecip%2Cprecipprob%2Cprecipcover%2Cpreciptype%2Csnow%2Csnowdepth%2Cwindgust%2Cwindspeed%2Cwinddir%2Cpressure%2Ccloudcover%2Csunrise%2Csunset%2Cmoonphase&include=days&key=" + key + "&maxStations=1&contentType=json"
-    
+    url = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/Southampton/" + start_date + "/" + end_date +"?unitGroup=metric&elements=datetime%2CdatetimeEpoch%2Cname%2Caddress%2Clatitude%2Clongitude%2Ctempmax%2Ctempmin%2Ctemp%2Cfeelslikemax%2Cfeelslikemin%2Cfeelslike%2Chumidity%2Cprecip%2Cprecipprob%2Cprecipcover%2Cpreciptype%2Csnow%2Csnowdepth%2Cwindgust%2Cwindspeed%2Cwinddir%2Cpressure%2Ccloudcover%2Csunrise%2Csunset%2Cmoonphase&include=days&key=" + key + "&maxStations=1&contentType=json"
     res = pd.read_json(url)
 
     data = []
@@ -18,6 +23,7 @@ def main():
         data.append(res['days'][i])
 
     df = pd.DataFrame(data)
+    df['datetime'] = pd.to_datetime(df['datetime'], format="%Y-%m-%d", errors='coerce')
     print(df)
     return df
 
