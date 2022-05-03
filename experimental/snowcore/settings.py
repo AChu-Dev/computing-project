e8834c5e6631b6b80854ab209e51c510ff90fed3
AChu-Dev <adamchu2001@gmail.com>
2022-05-03 13:21:49 +0100

    plz

--- experimental/snowcore/settings.py
+++ experimental/snowcore/settings.py
@@ -88,7 +88,7 @@ TEMPLATES = [
 
 STATICFILES_DIRS = [
     BASE_DIR / "static",
-    BASE_DIR / "staticfiles"
+    #BASE_DIR / "staticfiles"
 ]
 
 
@@ -142,10 +142,11 @@ USE_TZ = True
 
 WHITENOISE_USE_FINDERS = True
 
+if not DEBUG:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 #print(PROJECT_ROOT)
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
