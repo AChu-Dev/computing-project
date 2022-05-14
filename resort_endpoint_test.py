74872b8b91046e2ad91227b27670d1a5c72f482e
DARAEDom <drgn.domdev@gmail.com>
2022-05-14 23:53:32 +0100

    Added tests for Resort API

---
+++ resort_endpoint_test.py
@@ -0,0 +1,14 @@
+import requests
+
+datav2 = {'name': ['Three Valleys'], 'address': ['France'], 'longitude': ['-50.32'], 'latitude': ['32.2'], 'description': ['This is a nice palce']}
+response = requests.post("http://127.0.0.1:8000/rest_api/resort/", data = datav2)
+print(response)
+print(response.json())
+
+response_get = requests.get("http://127.0.0.1:8000/rest_api/resort/")
+print(response_get)
+print(response_get.json())
+
+response_delete = requests.delete("http://127.0.0.1:8000/rest_api/resort/", data={"pk": "3"})
+print(response_delete)
+print(response_delete.json())
