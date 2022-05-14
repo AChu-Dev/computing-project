d8b996d346ffe9d9f264281e13a631101ed1583b
AChu-Dev <adamchu2001@gmail.com>
2022-05-14 20:37:16 +0100

    plz overwrite

--- API/tests.py
+++ API/tests.py
@@ -9,34 +9,35 @@ import datetime
 logger = logging.getLogger(__name__)
 
 # Create your tests here.
-class userTests(unittest.TestCase):
+class ResortTests(unittest.TestCase):
     def setupTestObjects(self):
+        self.client = Client()
         logger.debug('Adding Users to DB for testing, DateTime:${}'.format(datetime.datetime.now()))
         for x in range (0, 3):
-            test_create_user(x)
+            self.test_create_resort(x)
 
-    def test_list_users(self):
+    def test_list_resorts(self):
         logger.debug('Test(API Endpoint): LIST All Users')
-        self.setupTestObjects()
-        
-        response = self.client.post("rest_api/user/list", follow=True)
+        self.setupTestObjects() 
+        response = self.client.post("rest_api/resort/list", follow=True)
         #logger.debug('Test(API Endpoint): LIST All Users, Response:${}, Status_Code:${}'.format(response.json(), response.status_code))
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.status_code, 200)
     
-    def test_create_user(self, x):
+    def test_create_resort(self, x):
         logger.debug('Test(API Endpoint): CREATE New User')
         
         data = {
                 'name':'Test{}'.format(x), 
-                'firstName':'Test{}_F'.format(x),
-                'lastName':'Test{}_S'.format(x),
-                'email':'test{}@snowcore.org.uk'.format(x),
-                'password':'password'
+                'address':'{} Test, Test, The Three Valleys'.format(x),
+                'lon':'{}'.format(45.4203 + x),
+                'lat':''.format(6.61409 + x)
                 }
         
-        response = self.client.post("/rest_api/user/create", data,format='json', follow=True)
+        response = self.client.post("/rest_api/resort/create", data,format='json', follow=True)
         #logger.debug('Test(API Endpoint): CREATE New User, Response:${}, Status_Code:${}'.format(response.json(), response.status_code))
-        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(response.status_code, 201)
+    
+    
         
     
 
