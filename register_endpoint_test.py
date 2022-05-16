01e3570bd636378bbefb2407fddcb8a3bd5ef34c
AChu-Dev <adamchu2001@gmail.com>
2022-05-16 12:12:00 +0100

    test folder made

--- register_endpoint_test.py
+++ register_endpoint_test.py
@@ -1,4 +1,48 @@
 import requests
+import os
+import shutil
+
+os.remove('db.sqlite3')
+shutil.copy('db_backup/db.sqlite3', './')
+
+def assertEqual(call, code):
+    if (call.status_code == code):
+        print('TEST SUCCEED')
+    else: 
+        print('TEST FAILED')
+
+
+datav2 = {
+        'name': ['Three Valleys: Test 1v2'],
+        'address': ['France'],
+        'longitude': ['-50.32'],
+        'latitude': ['32.2'],
+        'description': ['This is a nice place'],
+        "image": [""]}
+
+
+print('------------------------------------------------------')
+print('CREATE TEST - RESORT')
+for x in range(0, 3):
+    datav1 = {
+            'name': ['Three Valleys: Test {}'.format(x)],
+            'address': ['France'],
+            'longitude': ['-50.32'],
+            'latitude': ['32.2'],
+            'description': ['This is a nice palce'],
+            "image": [""]}
+    print(datav1)
+    response_post = requests.post("http://127.0.0.1:8000/rest_api/resort/", data = datav1)
+    print(response_post.status_code)
+    assertEqual(response_post, 201)
+print('------------------------------------------------------')
+
+print('LIST TEST - RESORT')
+response_get = requests.get("http://127.0.0.1:8000/rest_api/resort/")
+print(response_get.status_code)
+print(response_get.json())
+assertEqual(response_get, 200)
+print('--------------------------------------------------
 
 signup0 = {"username":["Joe"], "email":["Joe@Appleton.co.uk"], "password1": ["JoeAppleton"]}
 response0 = requests.post("http://127.0.0.1:8000/rest_api/duser/create/", data = signup0)
